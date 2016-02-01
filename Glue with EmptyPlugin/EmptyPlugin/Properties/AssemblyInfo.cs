260d4c06087551b0c179095b266e8c97b2c850df
stitch1280 <blay4480@gmail.com>
2016-01-31 22:19:26 -0700

    Committing changes. Getting weirdness in this reopo. Will check it tomorrow.

--- Glue with EmptyPlugin/EmptyPlugin/Properties/AssemblyInfo.cs
+++ Glue with EmptyPlugin/EmptyPlugin/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EmptyPluginProject")]
+[assembly: AssemblyTitle("EmptyPlugin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EmptyPluginProject")]
+[assembly: AssemblyProduct("EmptyPlugin")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("539fc66a-f7c4-40af-ad63-516aee805e13")]
+[assembly: Guid("2528fd4d-3fce-461f-bc38-08dff4d214e2")]
 
 // Version information for an assembly consists of the following four values:
 //
