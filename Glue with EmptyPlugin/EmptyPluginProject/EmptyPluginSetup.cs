178bee58e0e94688653803de500bbc5a016cc1ec
stitch1280 <blay4480@gmail.com>
2016-01-31 21:32:03 -0700

    Resolving issues with the new glue solution

---
+++ Glue with EmptyPlugin/EmptyPluginProject/EmptyPluginSetup.cs
@@ -0,0 +1,49 @@
+﻿using FlatRedBall.Glue.Plugins;
+using FlatRedBall.Glue.Plugins.ExportedInterfaces;
+using FlatRedBall.Glue.Plugins.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmptyPluginProject
+{
+    [Export(typeof(PluginBase))]
+    public partial class EmptyPluginSetup : PluginBase
+    {
+        [Import("GlueCommands")]
+        public IGlueCommands GlueCommands
+        {
+            get;
+            set;
+        }
+
+        [Import("GlueState")]
+        public IGlueState GlueState { get; set; }
+
+        public override string FriendlyName
+        {
+            get { return "Empty Plugin"; }
+        }
+
+        public override Version Version
+        {
+            get { return new Version(1, 0); }
+        }
+
+
+        public override void StartUp()
+        {
+
+        }
+
+        public override bool ShutDown(PluginShutDownReason shutDownReason)
+        {
+
+
+            return true;
+        }
+    }
+}
